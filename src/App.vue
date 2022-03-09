a4cb74da82e34321eb7b9c59a175cfe79237ba10
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-03-09 11:15:01 +0530

    basic structure - college

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- div l;ayout
+div ;left -- navbar
+div top div 
+div content
+
+ -->
+
+ <div class="menu-sidebar">
+  <NavMenu student-name="Issebella ava">
+ </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavMenu from './components/NavMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavMenu
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,51 @@
+<template>
+    
+    <div class="profile">
+        <img src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg" alt="student picture">
+        <h3> {{ student_name }} </h3>
+        <p> S7 - 51 </p>
+    </div>
+    <ul>
+        <li>
+            <a href="../lab/index.html" class="active">
+                <span class="icon"><i class="fas fa-desktop"></i></span>
+                <span class="item">Lab</span>
+            </a>
+        </li>
+        <li>
+            <a href="../exam/index.html">
+                <span class="icon"><i class="far fa-keyboard"></i></span>
+                <span class="item">Exam</span>
+            </a>
+        </li>
+        <li>
+            <a href="../resources/index.html">
+                <span class="icon"><i class="fas fa-book"></i></span>
+                <span class="item">Resources</span>
+            </a>
+        </li>
+        <li>
+            <a href="../profile/settings.html">
+                <span class="icon"><i class="fas fa-cog"></i></span>
+                <span class="item">Profile Settings</span>
+            </a>
+        </li>
+        <li>
+            <a href="../../index.html">
+                <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
+                <span class="item">Logout</span>
+            </a>
+        </li>
+    </ul>
+    
+</template>
+
+<script>
+export default {
+    name: 'NavMenu',
+    props: {
+        studentname: '',
+        items: []
+    }
+}
+</script>
