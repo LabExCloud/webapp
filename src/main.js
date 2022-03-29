d8e39cc4ca07822159bf0fbf61d63750f379e8a8
Akshaylal <akshaylals2000@gmail.com>
2022-03-29 08:24:31 +0530

    fixed fontawsome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone, faDesktop, faKeyboard, faBook, faCog, faSignOutAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(router).mount('#app')
+library.add(faPhone, faDesktop, faKeyboard, faBook, faCog, faSignOutAlt, faSearch);
+
+
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
