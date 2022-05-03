551c9cbe3c27e1af1f12530f1060530ad6dce7c2
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-05-03 11:32:16 +0530

    ..

--- src/apps/teacher/views/Exam.vue
+++ src/apps/teacher/views/Exam.vue
@@ -2,23 +2,21 @@
 
     <div class="content">
         <div class="header">
-            <p> Exams </p>
+            <p> Lab Exams </p>
         </div>
         
-        <!-- <div class="search-sort-box">
-                <input class="text-center py-2 w-72 bg-gray-700 border border-black rounded-xl" type="search" id="search" placeholder="Search Exams"/>
-                <div class="sort-box">
-                    <label class="text-white" for="sort">sort: </label>
-                    <select class="text-center text-sm text-gray-400 bg-gray-700 border border-black rounded" id="sort" name="sort">
-                        <option value="recent">Recent</option>
-                        <option value="star">Starred</option>
-                        <option value="date">Date</option>
-                        <option value="alph">Alphabet</option>
-                    </select>
-                </div>   
+        <div class="search-sort-box">
+            <input class="text-center py-2 w-72 bg-gray-700 border border-black rounded-xl" type="search" id="search" placeholder="Search Exams"/>
+            <div class="add-box">
+                <button class="text-center text-base w-40 text-white bg-gray-700 border border-black rounded-md">
+                    <router-link to="/demo/addlab">
+                        <span class="mx-4">+ New Exam</span>
+                    </router-link>
+                </button>
+            </div>
         </div>
 
-        <div class="grid grid-cols-2 gap-10 py-14 px-8 text-white">
+        <!-- <div class="grid grid-cols-2 gap-10 py-14 px-8 text-white">
             <div class="h-36 border border-borderclr rounded-2xl bg-cardclr">item 1</div>
             <div class="h-36 border border-borderclr rounded-2xl bg-cardclr">item 2</div>
             <div class="h-36 border border-borderclr rounded-2xl bg-cardclr">item 3</div>
--- src/apps/teacher/views/Labs.vue
+++ src/apps/teacher/views/Labs.vue
@@ -1,23 +1,21 @@
 <template>
     <div class="content">
         <div class="header">
-            <p> Labs </p>
+            <p> Lab Experiments </p>
         </div>
 
-        <!-- <div class="search-sort-box">
-                <input class="text-center py-2 w-72 bg-gray-700 border border-black rounded-xl" type="search" id="search" placeholder="Search Labs"/>
-                <div class="sort-box">
-                    <label class="text-white" for="sort">sort: </label>
-                    <select class="text-center text-sm text-gray-400 bg-gray-700 border border-black rounded" id="sort" name="sort">
-                        <option value="recent">Recent</option>
-                        <option value="star">Starred</option>
-                        <option value="date">Date</option>
-                        <option value="alph">Alphabet</option>
-                    </select>
-                </div>   
+        <div class="search-sort-box">
+            <input class="text-center py-2 w-72 bg-gray-700 border border-black rounded-xl" type="search" id="search" placeholder="Search Exps"/>
+            <div class="add-box">
+                <button class="text-center text-base w-40 text-white bg-gray-700 border border-black rounded-md">
+                    <router-link to="/demo/addlab">
+                        <span class="mx-4">+ New Exp</span>
+                    </router-link>
+                </button>
+            </div>
         </div>
 
-        <div class="grid grid-cols-2 gap-10 py-14 px-8 text-white">
+        <!-- <div class="grid grid-cols-2 gap-10 py-14 px-8 text-white">
             <LabItem/>
             <div class="h-36 border border-borderclr rounded-2xl bg-cardclr">item 1</div>
             <div class="h-36 border border-borderclr rounded-2xl bg-cardclr">item 2</div>
@@ -32,7 +30,7 @@
 export default({
     name: 'Labs',
     mounted(){
-        document.title = 'Labs'
+        document.title = 'Lab'
     },
     // data(){
     //     return{
