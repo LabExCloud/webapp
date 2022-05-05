4bc7ed0161f0216cc53f96f3cd4dc3425cea5061
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-05-05 08:05:27 +0530

    student labitem style , link.

--- src/apps/student/components/LabItem.vue
+++ src/apps/student/components/LabItem.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="lab-item" @click="cardClick">
-        <div>
-            <img :src="lab.subject.image" alt="subject image">
-            <div>
+    <div class="lab-item cursor-pointer" @click="clickCard(lab.id)">
+        <div class="flex  ml-4 mt-4">
+            <img :src="lab.subject.image" alt="subject image" class="h-24 w-24 rounded-2xl">
+            <div class="w-full flex flex-col px-6">
                 <p class="title">{{ lab.subject.sub_code }} - {{ lab.subject.sub_name }}</p>
-                <p class="name">{{ lab.owner.first_name + " " + lab.owner.last_name }}</p>
+                <p class="name text-gray-400"> - {{ lab.owner.first_name + " " + lab.owner.last_name }}</p>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
         </div>
 
-        <div class="w-full px-4">
-            <div class="bg-gray-200 h-1.5 rounded">
+        <div class="w-full px-4 py-5">
+            <div class="bg-gray-400 h-1.5 rounded">
                 <div class="bg-blue-600 h-1.5 rounded" style="width: 45%"></div>
             </div>
             <div class="flow-root">
@@ -27,8 +30,8 @@ export default({
         lab: {}
     },
     methods: {
-        cardClick(){
-            // this.$router.push()
+        clickCard(id){
+            this.$router.push(`/labs/${id}`)
         }
     }
 })
