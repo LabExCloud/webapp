2ee3754543f4af199978fba8ad77927b9602cebc
Akshaylal <akshaylals2000@gmail.com>
2022-05-06 10:57:11 +0530

    SemesterSelectBox Component

---
+++ src/apps/student/components/SemesterSelectBox.vue
@@ -0,0 +1,39 @@
+<template>
+    <select class="text-center text-sm text-gray-400 bg-gray-700 border border-black rounded" id="sem" name="sem" @change="selectSem">
+        <option v-for="o in semSelectBox" :key="o.sem" :value="o.sem">{{ o.text }}</option>
+    </select>
+</template>
+
+<script>
+export default({
+    name: 'SemesterSelectBox',
+    data(){
+        return {
+            semSelectBox: [],
+        }
+    },
+    props: {
+        sems: [],
+    },
+    mounted() {
+        const sems = this.sems.sort((a, b) => a < b)
+        sems.forEach(sem => {
+            this.semSelectBox.push({
+                sem,
+                text: `S${sem}`
+            })
+        });
+        this.semSelectBox.push({
+            sem: 0,
+            text: 'All'
+        })
+    },
+    methods: {
+        selectSem(event){
+            const sem = parseInt(event.target.value)
+            const sobj = this.semSelectBox.find(o => o.sem == sem)
+            this.$emit('selectSem', sobj)
+        }
+    }
+})
+</script>
