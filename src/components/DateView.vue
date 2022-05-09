1c370248bdcf0ddf3716dc4088ab476fecb6582d
Akshaylal <akshaylals2000@gmail.com>
2022-05-09 20:06:41 +0530

    date component

---
+++ src/components/DateView.vue
@@ -0,0 +1,35 @@
+<template>
+    <p>{{ date }}</p>
+</template>
+
+<script>
+export default {
+    name: "DateView",
+    data(){
+        return {
+            date: '',
+            monthNames: ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"],
+        }
+    },
+    mounted(){
+        this.formatDate()
+    },
+    watch: {
+        modelValue(){
+            this.formatDate()
+        }
+    },
+    props: {
+        modelValue: {
+            type: String,
+            required: true
+        },
+    },
+    methods: {
+        formatDate(){
+            const date = new Date(this.modelValue)
+            this.date = `${this.monthNames[date.getMonth()]} ${date.getDate()}`
+        },
+    }
+}
+</script>
