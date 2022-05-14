773c593af089403e981a384827657020e793baad
Akshaylal <akshaylals2000@gmail.com>
2022-05-14 19:07:06 +0530

    ProfileImage component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,28 @@
+<template>
+    <img :class="class" :src="profileImage" :alt="alt">
+</template>
+
+<script>
+export default {
+    name: "ProfileImage",
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        class: {
+            type: String,
+            default: 'w-28'
+        },
+        alt: {
+            type: String,
+            default: 'profile picture'
+        }
+    },
+    computed: {
+        profileImage() {
+            return process.env.VUE_APP_ROOT_MEDIA + this.src
+        }
+    },
+}
+</script>
