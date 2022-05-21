7c606678112eb5a3fc557af8ce5f6ee5609df51b
Akshaylal <akshaylals2000@gmail.com>
2022-05-21 22:54:26 +0530

    text color

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="show" class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-        <div class="bg-cardclr border border-borderclr px-16 py-14 rounded-2xl text-center">
+        <div class="bg-cardclr text-white border border-borderclr px-16 py-14 rounded-2xl text-center">
             <slot name="content">content</slot><br>
             <button class="bg-indigo-500 px-4 py-2 rounded-md text-md text-white" @click="$emit('cancel')"><slot name="cancel"></slot></button>
             <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold" @click="$emit('confirm')"><slot name="confirm"></slot></button>
