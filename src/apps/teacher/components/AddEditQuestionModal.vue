c9d17dadebd2536421cd44d7bff6718aa2b2f479
Akshaylal <akshaylals2000@gmail.com>
2022-05-23 01:04:15 +0530

    add and delete questions

--- src/apps/teacher/components/AddEditQuestionModal.vue
+++ src/apps/teacher/components/AddEditQuestionModal.vue
@@ -4,7 +4,7 @@
         
         <h1 class="text-xl"> {{ header }} </h1><br>
 
-        <form class="w-full pr-12 pt-10">
+        <form class="w-full pr-12 pt-10" @submit.prevent>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                 <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="questionNo">Question No: 
@@ -16,11 +16,21 @@
             </div>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
+                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="title">Question Title
+                </label>
+                </div>
+                <div class="md:w-2/3">
+                <input type="text" name="title" v-model="formData.title" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name">
+                </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
                 <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="question">Question: 
                 </label>
                 </div>
                 <div class="md:w-2/3">
-                <textarea name="question" cols="30" rows="10" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" ></textarea>
+                <textarea name="question" cols="30" rows="10" :value="formData.question" @input="formData.question = $event.target.value"
+                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" ></textarea>
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
@@ -84,7 +94,7 @@
                 <div class="md:w-2/3">
                 
                 <button class="bg-indigo-500 px-4 py-2 rounded-md text-md text-white" @click="$emit('cancel')"> Cancel </button>
-                <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold" @click="$emit('confirm')"> Confirm </button>
+                <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold" @click="saveQuestion"> Save </button>
                 </div>
             </div>
         </form>
@@ -121,8 +131,9 @@ export default({
     data(){
         return {
             formData: {
+                title: '',
                 questionNumber: Number,
-                question: String,
+                question: '',
                 language: Number,
                 mark: Number,
             },
@@ -139,14 +150,25 @@ export default({
         }
     },
     async mounted(){
-        if(this.edit){
-            this.getQuestion(this.id)
-        }
+    },
+    watch: {
+        show(){
+            if(this.edit){
+                this.getQuestion(this.id)
+            }else{
+                this.formData.questionNumber = undefined
+                this.formData.question = ''
+                this.formData.language = undefined
+                this.formData.mark = undefined
+                this.testcases = []
+            }
+        },
     },
     methods: {
         async getQuestion(id){
             const response = await axios.get(`${this.apiUrl}/question/${id}`)
             this.formData.questionNumber = response.data.question_number
+            this.formData.title = response.data.title
             this.formData.question = response.data.question
             this.formData.language = response.data.language
             this.formData.mark = response.data.mark
@@ -156,6 +178,27 @@ export default({
                 testcase.inputUrl = process.env.VUE_APP_ROOT_MEDIA + testcase.input_file
                 testcase.outputUrl = process.env.VUE_APP_ROOT_MEDIA + testcase.output_file
             });
+        },
+        async saveQuestion(){
+            if (this.edit){
+                const response = await axios.put(`${this.apiUrl}/question/${this.id}`,{
+                    question_number: this.formData.questionNumber,
+                    title: this.formData.title,
+                    question: this.formData.question,
+                    language: this.formData.language,
+                    mark: this.formData.mark
+                })
+                this.$emit('saved')
+            }else{
+                const response = await axios.post(`${this.apiUrl}/question/${this.id}`,{
+                    question_number: this.formData.questionNumber,
+                    title: this.formData.title,
+                    question: this.formData.question,
+                    language: parseInt(this.formData.language),
+                    mark: this.formData.mark
+                })
+                this.$emit('saved')
+            }
         }
     }
 })
--- src/apps/teacher/components/LabExperimentCard.vue
+++ src/apps/teacher/components/LabExperimentCard.vue
@@ -4,17 +4,18 @@
 
         <span class="material-symbols-outlined cursor-pointer text-lg text-red-600 float-right mr-4 pt-2" @click="$emit('delete')">delete</span>
         <span class="material-symbols-outlined cursor-pointer text-lg text-blue-400 float-right mr-2 pt-2" @click="expEditModal.show = true">edit</span>
-        <button class="flex items-center mt-2 ml-5 bg-gray-600 rounded px-2 py-0 text-sm" @click="questionModal.show = true">
+        <button class="flex items-center mt-2 ml-5 bg-gray-600 rounded px-2 py-0 text-sm" @click="showAddQuestionModal">
             <span class="material-symbols-outlined cursor-pointer text-lg">add</span> <span> ADD </span> 
         </button>
         
         <ul class="px-6 py-4">
             <li v-for="(question, index) in exp.questions" :key="question.id">
-                {{question.id}} ) {{ question.question }} - {{ question.mark}} marks <button style="color: red; font-face:bold;" @click="showQuestionDeleteModal(index)">X</button>
+                <span class="cursor-pointer" @click="showEditQuestionModal(index)">{{question.id}} ) {{ question.question }} - {{ question.mark}} marks</span> 
+                <button style="color: red; font-face:bold;" @click="showQuestionDeleteModal(index)">X</button>
             </li>
         </ul>
     </div>
-    <add-edit-question-modal :show="questionModal.show" @cancel="questionModal.show = false" :id="questionModal.id" :edit="questionModal.edit" apiUrl="/api/v1/labs"/>
+    <add-edit-question-modal :show="questionModal.show" @cancel="questionModal.show = false" @saved="questionSaved" :id="questionModal.id" :edit="questionModal.edit" apiUrl="/api/v1/labs"/>
     <modal :show="expEditModal.show" @cancel="expEditModal.show = false" @confirm="editExp">
         <template #header>
             <h1>Edit Experiment</h1>
@@ -106,9 +107,13 @@ export default({
             this.questionModal.show = true
         },
         showAddQuestionModal(){
-            this.questionModal.id = undefined
+            this.questionModal.id = this.exp.id
             this.questionModal.edit = false
             this.questionModal.show = true
+        },
+        questionSaved(){
+            this.questionModal.show = false
+            this.getExps(this.exp_id)
         }
     },
     async mounted(){
