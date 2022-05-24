d0f1e94400ba43c5dab453ccd55cc888fb67e89e
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-05-24 22:47:43 +0530

    cancel save margin top.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,8 +3,8 @@
         <div class="bg-cardclr text-white border border-borderclr px-16 py-14 rounded-2xl text-center">
             <slot name="header">header</slot>
             <slot name="content">content</slot><br>
-            <button class="bg-indigo-500 px-4 py-2 rounded-md text-md text-white" @click="$emit('cancel')"><slot name="cancel"></slot></button>
-            <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold" @click="$emit('confirm')"><slot name="confirm"></slot></button>
+            <button class="bg-indigo-500 px-4 py-2 rounded-md text-md text-white mt-3" @click="$emit('cancel')"><slot name="cancel"></slot></button>
+            <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold mt-3" @click="$emit('confirm')"><slot name="confirm"></slot></button>
         </div>
     </div>
 </template>
