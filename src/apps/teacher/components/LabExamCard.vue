362aaa297df4533d18c6b1fdf990b1813c826fee
Akshaylal <akshaylals2000@gmail.com>
2022-05-24 09:09:29 +0530

    exam add edit delete

---
+++ src/apps/teacher/components/LabExamCard.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="min-h-36 border border-borderclr rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">
+        <p class="mx-4 mt-5 inline"> {{exam.exam_name}} - {{ exam.questions.length}} Questions. </p>
+        Start Date: <date-view v-model="exam.start_date"></date-view>
+        Due date: <date-view v-model="exam.due_date"></date-view>
+
+        <span class="material-symbols-outlined cursor-pointer text-lg text-red-600 float-right mr-4 pt-2" @click="$emit('delete')">delete</span>
+        <span class="material-symbols-outlined cursor-pointer text-lg text-blue-400 float-right mr-2 pt-2" @click="examEditModal.show = true">edit</span>
+        <button class="flex items-center mt-2 ml-5 bg-gray-600 rounded px-2 py-0 text-sm" @click="showAddQuestionModal">
+            <span class="material-symbols-outlined cursor-pointer text-lg">add</span> <span> ADD </span> 
+        </button>
+        
+        <ul class="px-6 py-4">
+            <li v-for="(question, index) in exam.questions" :key="question.id">
+                <span class="cursor-pointer" @click="showEditQuestionModal(index)">{{question.question_number}} ) {{ question.question }} - {{ question.mark}} marks</span> 
+                <button style="color: red; font-face:bold;" @click="showQuestionDeleteModal(index)">X</button>
+            </li>
+        </ul>
+    </div>
+    <add-edit-question-modal :show="questionModal.show" @cancel="questionModal.show = false" @saved="questionSaved" :id="questionModal.id" :edit="questionModal.edit" pageType="lab"/>
+    <modal :show="examEditModal.show" @cancel="examEditModal.show = false" @confirm="editExam">
+        <template #header>
+            <h1>Edit Exam</h1>
+        </template>
+        <template #content>
+
+            <div class="w-full">
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="name">Exam Name: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="name" type="text" v-model="examEditModal.name"> </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="marks">Total Marks: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="marks" type="number" v-model="examEditModal.marks">  </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"><label for="start">Start: </label></div>
+                    <div class="md:w-2/3"><input class="rounded bg-gray-600" name="start" type="date" v-model="examEditModal.start"></div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="due">Due Date: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="due" type="date" v-model="examEditModal.due"> </div>
+                </div>
+            </div>
+        </template>
+        <template #cancel>
+            Cancel
+        </template>
+        <template #confirm>
+            Save
+        </template>
+    </modal>
+    <modal :show="questionDeleteModal.show" @cancel="questionDeleteModal.show = false" @confirm="deleteQuestion()">
+        <template #header>
+            <h1>You sure you want to delete this question?</h1>
+        </template>
+        <template #content>
+            <h2>{{ exam.questions[questionDeleteModal.index].question }}</h2>
+        </template>
+        <template #cancel>
+            Cancel
+        </template>
+        <template #confirm>
+            Delete
+        </template>
+    </modal>
+</template>
+
+<script>
+import axios from 'axios'
+import Modal from '@/components/Modal.vue'
+import AddEditQuestionModal from './AddEditQuestionModal.vue'
+import DateView from '@/components/DateView.vue'
+
+export default({
+    name: 'LabExamCard',
+    data(){
+        return {
+            exam: {
+                questions: []
+            },
+            questionModal: {
+                show: false,
+                edit: false,
+                id: undefined,
+            },
+            examEditModal: {
+                show: false,
+                name: '',
+                marks: 0,
+                due: '',
+                start: '',
+            },
+            questionDeleteModal: {
+                show: false,
+                index: undefined,
+            }
+        }
+    },
+    props: {
+        exam_id: {
+            type: Number,
+            required: true,
+        }
+    },
+    watch: {
+        async exam_id(newVal){
+            await this.getExams(newVal)
+        }
+    },
+    methods: {
+        async getExams(id){
+            const response = await axios.get(`/api/v1/labexams/exam/${id}`)
+            this.exam = response.data
+            this.examEditModal.name = this.exam.exam_name
+            this.examEditModal.marks = this.exam.total_marks
+            this.examEditModal.due = this.exam.due_date.split('T')[0]
+            this.examEditModal.start = this.exam.start_date.split('T')[0]
+        },
+        async editExam(){
+            const response = await axios.put(`/api/v1/labexams/exam/${this.exam.id}`, {
+                exam_name: this.examEditModal.name,
+                total_marks: this.examEditModal.marks,
+                start_date: this.examEditModal.start,
+                due_date: this.examEditModal.due
+            })
+            this.exam = response.data
+            this.examEditModal.show = false
+        },
+        showEditQuestionModal(index){
+            this.questionModal.id = this.exam.questions[index].id
+            this.questionModal.edit = true
+            this.questionModal.show = true
+        },
+        showAddQuestionModal(){
+            this.questionModal.id = this.exam.id
+            this.questionModal.edit = false
+            this.questionModal.show = true
+        },
+        showQuestionDeleteModal(index){
+            this.questionDeleteModal.index = index
+            this.questionDeleteModal.show = true
+        },
+        questionSaved(){
+            this.questionModal.show = false
+            this.getExams(this.exam_id)
+        },
+        async deleteQuestion(){
+            const response = axios.delete(`/api/v1/labexams/question/${this.exam.questions[this.questionDeleteModal.index].id}`)
+            this.exam.questions.splice(this.questionDeleteModal.index, 1)
+            this.questionDeleteModal.show = false
+        }
+    },
+    async mounted(){
+        await this.getExams(this.exam_id)
+    },
+    components: {
+        AddEditQuestionModal,
+        Modal,
+        DateView,
+    }
+})
+</script>
--- src/apps/teacher/views/exams/Exam.vue
+++ src/apps/teacher/views/exams/Exam.vue
@@ -8,39 +8,135 @@
         <div class="search-sort-box">
             <input class="text-center py-2 w-72 bg-gray-700 border border-black rounded-xl" type="search" id="search" placeholder="Search Exams"/>
             <div class="add-box">
-                <button class="text-center text-base w-40 text-white bg-gray-700 border border-black rounded-md">
-                    <router-link to="/demo/addlab">
-                        <span class="mx-4">+ New Exam</span>
-                    </router-link>
+                <button class="text-center text-base w-40 text-white bg-gray-700 border border-black rounded-md" @click="examAddModal.show = true">
+                    <span class="mx-4">+ New Exam</span>
                 </button>
             </div>
         </div>
 
-        <!-- <div class="grid grid-cols-2 gap-10 py-14 px-8 text-white">
-            <div class="h-36 border border-borderclr rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">item 1</div>
+        <div class="grid grid-cols-1 gap-10 py-14 px-8 text-white">
+            <lab-exam-card v-for="(exam, index) in exams" :key="exam.id" :exam_id="exam.id" @delete="showDeleteExamModal(index)"/>
+
             <div class="h-36 border border-borderclr rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">item 2</div>
             <div class="h-36 border border-borderclr rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">item 3</div>
             <div class="h-36 border border-borderclr rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">item 4</div>
-        </div> -->
+        </div>
     </div>
+
+    <modal :show="examDeleteModal.show" @cancel="examDeleteModal.show = false" @confirm="deleteExam">
+        <template #header>
+            <h1>You sure you want to delete this Exam?</h1>
+        </template>
+        <template #content>
+            <h2>{{ exams[this.examDeleteModal.index].exam_name }}</h2>
+        </template>
+        <template #cancel>
+            Cancel
+        </template>
+        <template #confirm>
+            Delete
+        </template>
+    </modal>
+    <modal :show="examAddModal.show" @cancel="examAddModal.show = false" @confirm="addExam">
+        <template #header>
+            <h1>Add new Exam</h1>
+        </template>
+        <template #content>
+
+            <div class="w-full">
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="name">Exam Name: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="name" type="text" v-model="examAddModal.name"> </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="marks">Total Marks: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="marks" type="number" v-model="examAddModal.marks">  </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"><label for="start">Start: </label></div>
+                    <div class="md:w-2/3"><input class="rounded bg-gray-600" name="start" type="date" v-model="examAddModal.start"></div>
+                </div>
+
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3"> <label for="due">Due Date: </label> </div>
+                    <div class="md:w-2/3"> <input class="rounded bg-gray-600" name="due" type="date" v-model="examAddModal.due"> </div>
+                </div>
+            </div>
+        </template>
+        <template #cancel>
+            Cancel
+        </template>
+        <template #confirm>
+            Create
+        </template>
+    </modal>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import axios from 'axios'
+
+import Modal from '@/components/Modal.vue'
+import LabExamCard from '../../components/LabExamCard.vue'
 
 export default({
     name: 'Exam',
     mounted(){
         document.title = 'Exam'
+        this.getExams()
     },
-    // data(){
-    //     return{
-    //         objectItems: {
-    //             key1: 'item one',
-    //             key2: 'item two',
-    //             key3: 'item three',
-    //             key4: 'item four'
-    //         }
-    //     }
-    // }
+    components: {
+        Modal,
+        LabExamCard,
+    },
+    data(){
+        return{
+            exams: [],
+            examAddModal: {
+                name: '',
+                marks: 0,
+                due: undefined,
+                start: undefined,
+                show: false,
+            },
+            examDeleteModal: {
+                show: false
+            }
+        }
+    },
+    methods: {
+        async getExams(){
+            var url = `/api/v1/labexams/${this.class_id}`;
+
+            const response = await axios.get(url);
+            this.exams = response.data;
+        },
+        async addExam(){
+            const response = await axios.post(`/api/v1/labexams/exam/${this.class_id}`, {
+                exam_name: this.examAddModal.name,
+                total_marks: this.examAddModal.marks,
+                start_date: this.examAddModal.start,
+                due_date: this.examAddModal.due
+            })
+            this.exams.push(response.data)
+            this.examAddModal.show = false
+        },
+        showDeleteExamModal(index){
+            this.examDeleteModal.index = index
+            this.examDeleteModal.show = true
+        },
+        async deleteExam(){
+            const response = await axios.delete(`/api/v1/labexams/exam/${this.exams[this.examDeleteModal.index].id}`)
+            this.exams.splice(this.examDeleteModal.index, 1)
+            this.examDeleteModal.show = false
+        }
+    },
+    computed: {
+        ...mapGetters([
+            'class_id',
+        ]),
+    }
 })
 </script>
