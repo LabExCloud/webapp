b583b254b388c22817cb8b891846c97f058c7ba8
Akshaylal <akshaylals2000@gmail.com>
2022-05-24 10:30:42 +0530

    remove navigation from profile

--- src/apps/teacher/TeacherApp.vue
+++ src/apps/teacher/TeacherApp.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="h-screen mainpage flex justify-center items-center font-snig text-base">
-    <Navigation v-if="$route.name !== 'home'"/>
+    <Navigation v-if="$route.name !== 'home' && $route.name !== 'profile'"/>
     
     <router-view/>
 </div>
