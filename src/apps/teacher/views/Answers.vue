1b2de5153e3f938b8158f3a99d34b2739a0c64cd
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-05-26 23:56:33 +0530

    answers css.

--- src/apps/teacher/views/Answers.vue
+++ src/apps/teacher/views/Answers.vue
@@ -4,12 +4,26 @@
         <p>Answers</p>
     </div>
 
-    <div v-for="(answer, index) in answers">
-        <div>{{ answer.answer }}</div>
-        <div><date-view v-model="answer.submitted" /></div>
-        <div><date-view v-model="answer.modified"></date-view></div>
-        <div>{{ answer.total_marks }}</div>
+    <div class="grid grid-cols-6 gap-y-2 gap-x-2 py-5 px-4 min-h-36 text-white text-lg">
+            <span> Qn: </span>
+            <span> Name </span>
+            <span> Answers </span>
+            <span> Submitted date </span>
+            <span> Modified date </span>
+            <span> Total Marks </span>
+        </div>
+
+    <div v-for="(answer, index) in answers" class="grid grid-cols-1 gap-1 py-2 px-2 text-white">
+        <div class="grid grid-cols-6 gap-y-2 gap-x-2 py-5 px-4 min-h-36  rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">
+            <span> {{ answer.question }}</span>
+            <span> {{ answer.student.first_name }} {{ answer.student.last_name }}</span>
+            <button class="bg-gray-600 rounded" > <a :href="answer.answer"> View answer </a> </button>
+            <span> <date-view v-model="answer.submitted"></date-view> </span>
+            <span> <date-view v-model="answer.modified"></date-view> </span>
+            <span> {{ answer.total_marks }} </span>
+        </div>
     </div>
+
 </div>
 </template>
 
@@ -25,12 +39,20 @@ export default({
         }
     },
     async mounted() {
-        await this.getAnswers(this.$route.params.ansid)
+        await this.getAnswers(this.$route.params.ansid);
+        
     },
     methods: {
         async getAnswers(id){
             const response = await axios.get(`/api/v1/labs/answers/${id}`)
             this.answers = response.data
+            this.updateAnswer();
+        },
+        updateAnswer(){
+            for(let eachAns in this.answers){
+                this.answers[eachAns].answer = axios.defaults.baseURL + this.answers[eachAns].answer;
+            }
+                //console.log(this.answers)
         }
     },
     components:{
