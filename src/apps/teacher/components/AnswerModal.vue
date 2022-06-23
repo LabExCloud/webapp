a65cd36b4cb51a765868619a2a356152b58024c7
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-06-24 00:09:26 +0530

    answershowmodel css.

--- src/apps/teacher/components/AnswerModal.vue
+++ src/apps/teacher/components/AnswerModal.vue
@@ -1,8 +1,13 @@
 <template>
     <div v-if="show" class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-        <div class="bg-cardclr text-white border border-borderclr px-16 py-14 rounded-2xl text-center">
+        <div class="min-w-[40rem] min-h-[35rem] bg-cardclr text-white border border-borderclr px-16 py-14 rounded-2xl">
             <slot name="header">header</slot>
             <slot name="content">content</slot><br>
+
+            <div class="text-center">
+                <button class="bg-green-500 px-4 py-2 rounded-md text-md text-white mt-2 mb-1" @click="$emit('close')"><slot name="close"></slot></button>
+            </div>
+            
         </div>
     </div>
 </template>
--- src/apps/teacher/views/Answers.vue
+++ src/apps/teacher/views/Answers.vue
@@ -1,12 +1,40 @@
 <template>
 <div class="content">
 
-    <AnswerModal :show="AnsModal.show">
+    <AnswerModal :show="AnsModal.show" @close="AnsModal.show = false">
         <template #header>
-            <h1 class="text-xl text-gray-400">heading</h1>
+            <h1 class="text-xl text-gray-400  text-center">Answer</h1>
         </template>
         <template #content>
-            <h2> content </h2>
+           <div class="w-[32rem] h-96 bg-gray-800 overflow-auto rounded mt-6 p-8">
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
+                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+                    containing Lorem Ipsum passages, and more recently with 
+                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
+                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+                    containing Lorem Ipsum passages, and more recently with 
+                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
+                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+                    containing Lorem Ipsum passages, and more recently with 
+                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+        </template>
+        
+
+        <template #close>
+                Close
         </template>
 
     </AnswerModal>
