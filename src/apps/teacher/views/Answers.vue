04c587477acb36004c20c16d9763bc38120594d4
Akshaylal <akshaylals2000@gmail.com>
2022-06-24 11:38:18 +0530

    view answer in modal

--- src/apps/teacher/views/Answers.vue
+++ src/apps/teacher/views/Answers.vue
@@ -7,28 +7,7 @@
         </template>
         <template #content>
            <div class="w-[32rem] h-96 bg-gray-800 overflow-auto rounded mt-6 p-8">
-                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-                    containing Lorem Ipsum passages, and more recently with 
-                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-                    containing Lorem Ipsum passages, and more recently with 
-                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen 
-                    book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-                    containing Lorem Ipsum passages, and more recently with 
-                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
+                <p> {{ AnsModal.answer }} </p>
             </div>
         </template>
         
@@ -85,6 +64,7 @@ export default({
             AnsModal: {
                 CurrentAnsUrl: "",
                 show: false,
+                answer: '',
             },
         }
     },
@@ -107,7 +87,12 @@ export default({
         async showAnswerModal(Ansurl){
             this.AnsModal.show = true
             this.AnsModal.CurrentAnsUrl = Ansurl
-
+            try {
+                const response = await fetch(Ansurl)
+                this.AnsModal.answer = await response.text()
+            }catch(e){
+                console.log(e)
+            }
         },
     },
     components:{
