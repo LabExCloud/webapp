0494172316d43f5f0162b5c738d106f880b22c8a
Akshaylal <akshaylals2000@gmail.com>
2022-06-25 23:37:07 +0530

    password change teacher

--- src/apps/teacher/views/Profile.vue
+++ src/apps/teacher/views/Profile.vue
@@ -35,12 +35,48 @@
                     Password
                 </div>
                 <div class="bg-gray-800 rounded px-3 text-gray-300">
-                    *******
+                    <button @click="showChangePasswordModal">Change Password</button>
                 </div>   
             </div>
 
         </div>
     </div>
+    <modal :show="changePasswordModal.show" @cancel="changePasswordModal.show = false" @confirm="changePassword">
+        <template #header>
+            <h1 class="mb-4 text-xl text-gray-400">Change Password</h1>
+        </template>
+        <template #content>
+            <div class="w-full mt-4">
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3">
+                        <label for="pass1">New Password: </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="rounded bg-gray-600 text-black" name="pass1" type="password" v-model="changePasswordModal.password1">
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-2">
+                    <div class="md:w-1/3">
+                        <label for="pass2">Enter Again: </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="rounded bg-gray-600 text-black" name="pass2" type="password" v-model="changePasswordModal.password2">
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center items-center text-red-700">
+                    {{ changePasswordModal.error }}
+                </div>
+                        
+            </div>
+
+        </template>
+        <template #cancel>
+            Cancel
+        </template>
+        <template #confirm>
+            Change 
+        </template>
+    </modal>
 
 </template>
 
@@ -48,11 +84,13 @@
 <script>
 import axios from 'axios'
 import ProfileImage from '@/components/ProfileImage.vue'
+import Modal from '@/components/Modal.vue'
 
 export default({
     name: 'Profile',
     components: {
         ProfileImage,
+        Modal,
     },
     mounted(){
         document.title = 'Profile'
@@ -64,9 +102,32 @@ export default({
             user: {
                 profile: {}
             },
+            changePasswordModal: {
+                show: false,
+                password1: '',
+                password2: '',
+                error: '',
+            }
         }
     },
     methods: {
+        showChangePasswordModal(){
+            this.changePasswordModal.password1 = ''
+            this.changePasswordModal.password2 = ''
+            this.changePasswordModal.error = ''
+            this.changePasswordModal.show = true
+        },
+        async changePassword(){
+            if (this.changePasswordModal.password1 === this.changePasswordModal.password2){
+                this.changePasswordModal.error = ''
+                const response = await axios.put('/api/v1/password',{
+                    'password': this.changePasswordModal.password1
+                })
+                this.changePasswordModal.show = false
+            }else{
+                this.changePasswordModal.error = 'Passwords Does not match'
+            }
+        },
         getProfile(){
             axios({
                 method: 'get',
