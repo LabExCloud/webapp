d9b28b12052963093f89467bed96513a0be2e704
Akshaylal <akshaylals2000@gmail.com>
2022-06-26 21:29:54 +0530

    remove question id from answers

--- src/apps/teacher/views/Answers.vue
+++ src/apps/teacher/views/Answers.vue
@@ -30,8 +30,7 @@
 
     <button @click="getAnswers($route.params.ansid)" class="bg-gray-600 px-4 py-2 rounded m-5"><span class="material-symbols-outlined text-xl">refresh</span></button>
 
-    <div v-if="answers.length > 0" class="grid grid-cols-6 gap-y-2 gap-x-2 py-5 px-4 min-h-36 text-white text-lg">
-            <span> Qn: </span>
+    <div v-if="answers.length > 0" class="grid grid-cols-5 gap-y-2 gap-x-2 py-5 px-4 min-h-36 text-white text-lg">
             <span> Name </span>
             <span> Answers </span>
             <span> Submitted date </span>
@@ -41,8 +40,7 @@
     <div v-else class="text-white text-xl text-center"> Nothing to show ! </div>
 
     <div v-for="(answer, index) in answers" class="grid grid-cols-1 gap-1 py-2 px-2 text-white">
-        <div class="grid grid-cols-6 gap-y-2 gap-x-2 py-5 px-4 min-h-36  rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">
-            <span> {{ answer.question }}</span>
+        <div class="grid grid-cols-5 gap-y-2 gap-x-2 py-5 px-4 min-h-36  rounded-2xl shadow-md bg-cardclr hover:border-gray-300 hover:shadow-2xl">
             <span> {{ answer.student.first_name }} {{ answer.student.last_name }}</span>
             <button class="bg-gray-600 rounded" @click="showAnswerModal(answer.answer)"> View </button>
             <span> <date-view v-model="answer.submitted"></date-view> </span>
