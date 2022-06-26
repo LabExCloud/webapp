23e61a1de0325e3c671b7776ed7281c111820a57
Akshaylal <akshaylals2000@gmail.com>
2022-06-26 09:58:19 +0530

    removed favourite

--- src/apps/student/components/LabItem.vue
+++ src/apps/student/components/LabItem.vue
@@ -6,9 +6,6 @@
                 <p class="title">{{ lab.subject.sub_code }} - {{ lab.subject.sub_name }}</p>
                 <p class="name text-gray-400"> - {{ lab.owner.first_name + " " + lab.owner.last_name }}</p>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
         </div>
 
         <div class="w-full px-4 py-5">
